dcb069c16425835ff5ba20f951a77d244d1d3850
Onkar Ingale <ingale2273@gmail.com>
2020-06-03 00:08:22 +0530

    Optimize

--- addons/frac.cpp
+++ addons/frac.cpp
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<iostream>
 #include <math.h>
 #include <stdint.h>
 #include "frac.hpp"
+using namespace std;
 /* f : number to convert.
  * num, denom: returned parts of the rational.
  * md: max denominator value.  Note that machine floating point number
@@ -12,14 +14,14 @@
  *     2**52, which is in a sense accurate, but generally not very useful:
  *     1.0/7.0 would be "2573485501354569/18014398509481984", for example.
  */
-void rat_approx(double f, int64_t md, int64_t *num, int64_t *denom)
+void rat_approx(double f, int md, int *num, int *denom)
 {
 	/*  a: continued fraction coefficients. */
-	int64_t a, h[3] = { 0, 1, 0 }, k[3] = { 1, 0, 0 };
-	int64_t x, d, n = 1;
+	int a, h[3] = { 0, 1, 0 }, k[3] = { 1, 0, 0 };
+	int x, d, n = 1;
 	int i, neg = 0;
 
-	if (md <= 1) { *denom = 1; *num = (int64_t) f; return; }
+	if (md <= 1) { *denom = 1; *num = (int) f; return; }
 
 	if (f < 0) { neg = 1; f = -f; }
 
@@ -52,17 +54,38 @@ void rat_approx(double f, int64_t md, int64_t *num, int64_t *denom)
 void frac(double f1)
 {
 	int i;
-	int64_t d, n;
-	double f;
+	//int j;
+	int d, n;
+	float f;
+	float a = f1;
+	float b,c;
 	f=f1;
 
+
 	//printf("f = %16.14f\n", f);
-	//for (i = 1; i <= 20; i *= 16) {
-	i=16;
-	//	printf("denom <= %d: ", i);
+	for (i = 1; i <= 10000000; i *= 16) {
+	//i=16;
+	//	printf("\n denom <= %d: ", i);
 		rat_approx(f, i, &n, &d);
-		printf("       %lld/%lld\n", n, d);
-	//}
-
-
+		b=n;
+		c=d;
+		if(d==0||n==0)
+            continue;
+		a= (((float)n)/((float)d));
+		//printf("\n a : %f \n====\n n=%d \n d=%d\n==== \n n=%d \n d=%d \n====\n b=%f \n c=%f ",a,n,d,n,d,b,c);
+		//printf("\n n=%lld",n);
+		//printf("\n n=%lld",d);
+		//cout<<"\n ==**\t"<<b/c<<"\t**=="<<n/d<<"\n";
+		if(a == f)
+		{
+            printf("       %d/%d\n", n, d);
+            break;
+		}
+	}
+//	printf("\n Custom : \n");
+	//printf("\n Enter Custom j : ");
+	//scanf("%d",&j);
+   // rat_approx(f, j, &n, &d);
+  //  printf("j:%d       %d/%d\n",j, n, d);
+   // system("pause");
 }
